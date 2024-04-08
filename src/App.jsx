dedf0c70aeb4a4ec0f62e5a9379e8d0a6dbb40e9
Matheus Estevan <matheusetevan08@gmail.com>
2024-04-08 09:25:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HowlsMovin from './components/Comeco'
+import Conteudo from './components/Conteudo'
+import Cont from './components/Conteudo'
+
+export default function App() {
+  return (
+    <section>
+      <HowlsMovin />
+      <Conteudo />
+    </section>
+  )
+}
---
+++ src/components/Conteudo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Conteudo() {
+  return (
+    <section>
+        <h1 className='font-poppins font-extrabold uppercase'>Howl's Moving Castle</h1>
+        <p className='font-poppins font-normal mt-4'>Howl's Moving Castle (japonês: ハウルの動く城, Hepburn: Hauru no Ugoku Shiro) é um filme de animação japonês de fantasia de 2004 escrito e dirigido por Hayao Miyazaki. É vagamente baseado no romance homônimo de 1986, da autora britânica Diana Wynne Jones. O filme foi produzido por Toshio Suzuki, animado pelo Studio Ghibli e distribuído pela Toho.</p>
+    </section>
+  )
+}
+
+export default Conteudo
