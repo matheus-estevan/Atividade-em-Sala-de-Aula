85e981375dbda54d6a751bcf61743ff9b26fe6d8
Matheus Estevan <matheusetevan08@gmail.com>
2024-04-08 09:47:22 -0300

    Inicio do projeto com a imagem e a descrição do filme e sinopse

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -3,8 +3,24 @@ import React from 'react'
 function Conteudo() {
   return (
     <section>
-        <h1 className='font-poppins font-extrabold uppercase'>Howl's Moving Castle</h1>
-        <p className='font-poppins font-normal mt-4'>Howl's Moving Castle (japonês: ハウルの動く城, Hepburn: Hauru no Ugoku Shiro) é um filme de animação japonês de fantasia de 2004 escrito e dirigido por Hayao Miyazaki. É vagamente baseado no romance homônimo de 1986, da autora britânica Diana Wynne Jones. O filme foi produzido por Toshio Suzuki, animado pelo Studio Ghibli e distribuído pela Toho.</p>
+        <h1 className='font-poppins font-extrabold uppercase text-6xl mt-20'>Howl's Moving Castle</h1>
+        <p className='font-poppins font-normal mt-4 mb-10'>É um filme de animação japonês de fantasia de 2004 escrito e dirigido por Hayao Miyazaki. É vagamente baseado no romance homônimo de 1986, da autora britânica Diana Wynne Jones. O filme foi produzido por Toshio Suzuki, animado pelo Studio Ghibli e distribuído pela Toho.</p>
+
+        <h2 class="font-poppins text-5xl font-semibold mb-5">Sinopse:</h2>
+
+        <p class="font-poppins text-zinc-800 mb-10">O filme conta a história de Sophie, uma jovem que trabalha na chapelaria de sua família. Um dia, ela é transformada em uma velha senhora por uma bruxa ciumenta. Desesperada, Sophie foge e encontra um imponente castelo ambulante pertencente ao misterioso Howl, um mago cujo coração foi amaldiçoado e preso por um demônio de fogo. Dentro do castelo, ela faz amizade com Calcifer, o demônio que alimenta o castelo, e se torna a governanta de Howl. Através de suas aventuras e encontros com personagens fascinantes, Sophie embarca em uma jornada de autoconhecimento e descobre segredos sobre si mesma, Howl e o mundo mágico que os cerca.</p>
+
+        <h2 class="font-poppins text-5xl font-semibold mb-4">História:</h2>
+
+        <p class="font-poppins text-zinc-800 whitespace-pre-line mb-10">A história de "O Castelo Animado" é uma aventura mágica que segue a jornada de Sophie, uma jovem que trabalha em uma chapelaria e é transformada em uma velha senhora por uma bruxa ciumenta. Após essa transformação, Sophie embarca em uma jornada para encontrar uma cura para seu feitiço.<br></br>
+
+        Durante sua busca, Sophie encontra o castelo ambulante de Howl, um mago enigmático que possui um coração amaldiçoado. O castelo é uma maravilha mecânica e mágica, movida por Calcifer, um demônio de fogo com quem Sophie faz um pacto para se tornar a nova governanta do castelo. Dentro do castelo, ela conhece Markl, o aprendiz de Howl, e o próprio Howl, um mago habilidoso mas atormentado por seus próprios segredos e medos.<br></br>
+
+        Enquanto Sophie tenta desfazer sua própria maldição, ela se vê envolvida em uma guerra eminente entre dois reinos. Ela descobre segredos sombrios sobre Howl, incluindo seu passado traumático e sua ligação com o conflito que se aproxima. Através de suas aventuras e desafios, Sophie e Howl desenvolvem um vínculo especial, enfrentando juntos não apenas seus inimigos externos, mas também suas próprias inseguranças e medos.<br></br>
+
+        A história é um conto de amizade, coragem e autodescoberta, ambientado em um mundo de magia, máquinas e personagens cativantes.<br></br></p>
+
+
     </section>
   )
 }
